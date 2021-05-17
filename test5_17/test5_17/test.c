0f8bb4d679d357c40c19ea9ea023a837e0e0db4e
xyhccc <1223691914@qq.com>
2021-05-17 12:07:41 +0800

    5.17

---
+++ test5_17/test5_17/test.c
@@ -0,0 +1,90 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include<stdio.h>
+
+//int main()
+//{
+//	int arr[10][10] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		for (j = 0; j < 10; j++)
+//		{
+//			if (j == 0)
+//			{
+//				arr[i][j] = 1;
+//			}
+//			if (i == j)
+//			{
+//				arr[i][j] = 1;
+//			}
+//			if (i >= 2 && j >= 1)
+//			{
+//				arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
+//			}
+//
+//		}
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		for (j = 0; j <=i; j++)
+//		{
+//			printf("%d",arr[i][j]);
+//
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int kille = 0;
+//	for (kille = 'a'; kille <= 'd'; kille++)
+//	{
+//		if ((kille != 'a') + (kille == 'c') + (kille == 'd') + (kille != 'd')==3)
+//		{
+//			printf("%c\n", kille);
+//		}
+//	}
+//		return 0;
+//
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int d = 0;
+//	int e = 0;
+//	for (a = 1; a <= 5; a++)
+//	{
+//		for (b = 1; b <= 5; b++)
+//		{
+//			for (c = 1; c <= 5; c++)
+//			{
+//				for (d = 1; d <= 5; d++)
+//				{
+//					for (e = 1; e <= 5; e++)
+//					{
+//						if (((b == 2) + (a == 3) == 1) &&
+//							((b ==2) +  (e == 4) == 1) &&
+//							((c == 1) + (d == 2) == 1) &&
+//							((c == 5) + (d == 3) == 1) &&
+//							((e == 4) + (a == 1) == 1))
+//						{
+//							if (a*b*c*d*e == 120)
+//							{
+//								printf("a = %d, b = %d, c = %d, d = %d, e = %d\n", a, b, c, d, e);
+//
+//							}
+//							
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
+//}
+
