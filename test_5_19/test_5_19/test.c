6bc183ed09065b2d716da4bcf828617796da8b3b
xyhccc <1223691914@qq.com>
2021-05-19 15:00:33 +0800

    5.19

---
+++ test_5_19/test_5_19/test.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+////演示实例
+//int main()
+//{
+//	int n = 10;
+//	char *pc = (char*)&n;
+//	int *pi = &n;
+//
+//	printf("%p\n", &n);
+//	printf("%p\n", pc);
+//	printf("%p\n", pc + 1);
+//	printf("%p\n", pi);
+//	printf("%p\n", pi + 1);
+//	return 0;
+//}
+
+#include <stdio.h>
+int main()
+{
+	char a = -1;
+	signed char b = -1;
+	unsigned char c = -1;
+	printf("a=%d,b=%d,c=%d", a, b, c);
+	return 0;
+}
