5f0438e13aaacddf93ef706e0ea8075eb3f3b923
xyhccc <1223691914@qq.com>
2021-05-25 10:40:36 +0800

    通讯录改进动态内存

---
+++ Contact -dynamic/Contact/contact.h
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+//#define MAX 500
+#define DEFALT_SZ 3
+
+#define MAX_NAME 20
+#define MAX_SEX  5
+#define MAX_TELE 12
+#define MAX_ADDR 30
+
+enum Option
+{
+	EXIT,
+	ADD,
+	DEL,
+	SEARCH,
+	MODIFY,
+	SHOW
+
+};
+struct PeoInfo
+{
+	char name[MAX_NAME];
+	int age;
+	char sex[MAX_SEX];
+	char tele[MAX_TELE];
+	char addr[MAX_ADDR];
+};
+//通讯录类型
+struct Contact
+{
+	struct PeoInfo* data;
+	int size;//当前拥有的元素个数
+	int capcity;//最大容量
+};
+//初始化函数
+void InitContact(struct Contact* ps);
+//增加函数
+void AddContact(struct Contact* ps);
+//删除函数
+void DelContact(struct Contact* ps);
+//查找函数
+void SearchContact(struct Contact* ps);
+//修改函数
+void ModifyContact(struct Contact* ps);
+//显示函数
+void ShowContact(struct Contact* ps);
+//释放内存
+void DestroyContact(struct Contact* ps);
+
+
---
+++ Contact -dynamic/Contact/test.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"contact.h"
+
+void menu()
+{
+	printf("********************************\n");
+	printf("*********1.add       2.del  ****\n");
+	printf("*********3.search    4.modify***\n");
+	printf("*********5.show      0.exit ****\n");
+	printf("********************************\n");
+	printf("********************************\n");
+}
+
+int main()
+{
+	int inpute = 0;
+	struct Contact con;
+	//创建通讯录
+	//初始化
+	InitContact(&con);
+
+	do
+	{
+	menu();
+	printf("请输入");
+	scanf("%d", &inpute);
+		switch (inpute)
+		{
+		case ADD:
+			AddContact(&con);
+			break;
+		case DEL:
+			DelContact(&con);
+			break;
+		case SEARCH:
+			SearchContact(&con);
+			break;
+		case MODIFY:
+			ModifyContact(&con);
+			break;
+		case SHOW:
+			ShowContact(&con);
+			break;
+		case EXIT:
+			DestroyContact(&con);
+			printf("退出");
+			break;
+		default:
+			printf("选择错误\n");
+
+		}
+	} while (inpute);
+	return 0;
+}
