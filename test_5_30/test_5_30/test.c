8af5a4b35cc7737e25505d8c587ef4df62a83c8e
xyhccc <1223691914@qq.com>
2021-05-30 16:09:11 +0800

    5.30

---
+++ test_5_30/test_5_30/test.c
@@ -0,0 +1,94 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+
+//    1
+//   1 1
+//  1 1 1
+// 1 1 1 1
+//1 1 1 1 1
+#include<stdio.h>
+//int main()
+//{
+//	char ch = 0;
+//	scanf("%c", &ch);
+//	int i = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j <4-i; j++)
+//		{
+//			printf(" ");
+//		}
+//	
+//		for (j = 0; j < i + 1; j++)
+//		{
+//			printf("%c ",ch);
+//			
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	char ch = 0;
+//	scanf("%c", &ch);
+//	int i = 0;
+//	for (i = 0; i<5; i++)
+//	{
+//		//处理一行
+//		//空格
+//		int j = 0;
+//		for (j = 0; j<5 - i - 1; j++)
+//		{
+//			printf(" ");
+//		}
+//		//数字
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf("%c ", ch);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+/*nt main()
+{
+	char arr[] = { 73, 32, 99, 97, 110, 32, 100, 111, 32, 105, 116, 33 };
+	int i = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (i = 0; i < sz; i++)
+	{
+		printf("%c", arr[i]);
+	}
+	return 0;
+}*/
+//int main()
+//{
+//	int n = 0;
+//	while (scanf("%d", &n) != EOF)
+//	{
+//		printf("%d\n", 1 << n);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int tiz = 0;
+//	int sg = 0;
+//	scanf("%d%d", &tiz, &sg);
+//	double BMI = 0.00;
+//	BMI = tiz / (double)((sg*sg) / 10000);
+//	printf("%.2lf", BMI);
+//	return 0;
+//}
+//int main()
+//{
+//	int weight = 0;
+//	int height = 0;
+//	double bmi = 0.0;
+//	scanf("%d%d", &weight, &height);
+//	bmi = weight / ((double)height*height / 10000);
+//	printf("%.2lf\n", bmi);
+//	return 0;
+//}
