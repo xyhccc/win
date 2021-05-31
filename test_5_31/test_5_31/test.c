7235bf551d4df513e4eacc0d66bb2c821fa10db3
xyhccc <1223691914@qq.com>
2021-05-31 17:15:09 +0800

    5.31

---
+++ test_5_31/test_5_31/test.c
@@ -0,0 +1,42 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1 
+#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	int h = 0;
+//	int m = 0;
+//	while (scanf("%d %d %d", &n, &h, &m) != EOF)
+//	{
+//		if (m%h > 0)
+//		{
+//			printf("%d\n", n - m / h - 1);
+//		}
+//		else
+//			printf("%d\n", n - m / h);
+//
+//	}
+//	return 0;
+//}
+//Name   Age   Gender
+//--------------------21个
+//Jack   18     man和第一行对齐
+//int main()
+//{
+//	char arr[] = { 'a','b','c' };
+//	printf("%c", arr[1]);
+//	return 0;
+//}
+int main()
+{
+	int arr = 0;
+	int i = 0;
+	float tmp = 0;
+	for (i = 0; i < 5; i++)
+	{
+		scanf("%d", &arr);
+		tmp += arr;
+	}
+	printf("%.1f", tmp / 5);
+
+	return 0;
+}
