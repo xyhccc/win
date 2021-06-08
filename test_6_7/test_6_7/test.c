28651718b6a062269f00437fa41a5d8475085ea0
xyhccc <1223691914@qq.com>
2021-06-08 20:28:53 +0800

    6.8

---
+++ test_6_7/test_6_7/test.c
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int i = 0;
+//	int x = 0;
+//	int count = 0;
+//	int a[50] = { 0 };
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &a[i]);
+//	}
+//	scanf("%d", &x);
+//	for (i = 0; i < n; i++)
+//	{
+//		if (a[i] == x)
+//		{
+//			count++;
+//		}
+//	}
+//	printf("%d", count);
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[101] = { 0 };
+//	int n = 0;
+//	scanf("%d", &n);
+//	int i = 0;
+//	for (i = 0; i<n; i++)
+//	{
+//		int m = 0;
+//		scanf("%d", &m);
+//		arr[m]++;
+//	}
+//	int x = 0;
+//	scanf("%d", &x);
+//	printf("%d", arr[x]);
+//	return 0;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int i = 0;
+//	int tmp = 0;
+//	int a[1001] = { 0 };
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &tmp);
+//		a[tmp] = tmp;
+//	}
+//	for (i = 0; i < 1001; i++)
+//	{
+//		if (a[i] != 0)
+//		{
+//			printf("%d",a[i]);
+//		}
+//	}
+//	return 0;
+//}
